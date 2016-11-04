642e8ca310d8050089453253eb52dca00675d469
kirkkw <kirkkw@users.noreply.github.com>
2016-11-03 18:18:45 -0700

    Add files via upload

---
+++ inc/deletejsonhist.php
@@ -0,0 +1,9 @@
+<!-- Writing to JSON -->
+<?php 
+session_start();
+$myJSON = $_POST["myJSON"];
+$data = json_encode($myJSON, JSON_PRETTY_PRINT);
+$myfile = fopen("../json/mainlist.json", "w") or die("Unable to open file!");
+fwrite($myfile, $data);
+fclose($myfile);
+?>
